5030bca5fd42108973c3f9ab4cf86455f751e572
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-01-27 03:39:20 -0800

    new file and code added

---
+++ payoneer.js
@@ -0,0 +1,51 @@
+        // login button event handler
+        const enter = document.getElementById('login');
+        enter.addEventListener('click', function(){
+            const submitArea = document.getElementById('login-area');
+            submitArea.style.display = 'none';
+            const transactionArea = document.getElementById('transaction-area');
+            transactionArea.style.display = 'block';
+        })
+
+        // Deposit button event handler
+        const depositBtn = document.getElementById('deposit-btn');
+        depositBtn.addEventListener('click', function(){
+            const depositAmount = document.getElementById('deposit-amount').value;
+            const depositNumber = parseFloat(depositAmount);
+
+            updateSpanText('current-deposit', depositNumber);
+            updateSpanText('current-balance', depositNumber);
+
+            document.getElementById('deposit-amount').value = '';
+
+        })
+
+
+        // Withdraw button event handler
+        const withdrawBtn = document.getElementById('withdraw-btn');
+        withdrawBtn.addEventListener('click', function(){
+            const withdrawNumber = getInputNumber('withdraw-amount');
+            console.log(withdrawNumber)
+        })
+        
+        function getInputNumber(id){
+            const withdrawAmount = document.getElementById(id).value;
+            const withdrawNumber = parseFloat(withdrawAmount);
+            return withdrawNumber
+        }
+
+
+
+
+
+
+
+        function updateSpanText(id, depositNumber){
+            const current = document.getElementById(id).innerText;
+            const currentNumber = parseFloat(current);
+            const totalBalance = depositNumber + currentNumber;
+            document.getElementById(id).innerText = totalBalance;
+        }
+
+
+
